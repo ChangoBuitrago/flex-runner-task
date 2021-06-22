a88f171ce0c7bdf096effed972b5f2d5f0b133f2
Pablo Buitrago <pablo@briefboards.com>
2021-06-22 16:40:41 -0300

    body commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    "pyzmq==22.1.0",
+    "reindent==3.5.1",
+]
+
+setup(
+    name="flex-task",
+    author="Pablo Buitrago",
+    author_email="messaging@energyweb.org",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.9",
+    install_requires=install_requires,
+    extras_require={
+        "all": install_requires
+    }
+)
---
+++ src/task.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import asyncio
+import logging
+import threading
+from secrets import token_hex
+from typing import List
+
+from core.mdcliapi import MajorDomoClient
+from core.mdwrkapi import MajorDomoWorker
+
+# class ReqSocket():
+#     def __init__(self, name:str, endpoint:str):
+#         name:bytes = name.encode("utf-8")
+#         self.worker = MajorDomoClient(endpoint, name)
+
+#         #  1. Send 'name' request to Titanic
+#         request = [name, b"Hello world"]
+#         reply = self.service_call(self.worker, b"titanic.request", request)
+
+class RepSocket():
+    def __init__(self, name:str, endpoint:str):
+        name:bytes = name.encode("utf-8")
+        self.worker = MajorDomoWorker(endpoint, name)
+        request = self.worker.recv(None)
+        if request is None:
+            raise # Worker was interrupted
+
+    def recv(self, outputs:List, keep_alive:bool):
+        request = self.worker.recv(outputs, keep_alive)
+        if request is None:
+            raise Exception
+
+
+class CoreIO():
+    # req_socket:ReqSocket
+    rep_socket:RepSocket
+    keep_alive:bool
+
+    def __init__(self, name:str, keep_alive:bool) -> None:
+        self.rep_socket = RepSocket(
+            name="{}.outputs".format(name),
+            endpoint="tcp://localhost:5555"
+        )
+        self.keep_alive = keep_alive
+        
+    def get_inputs(self) -> List:
+        # Auth task
+        task_input = {
+            "address":"0x{}".format(token_hex(40)),
+            "method": "ethr"
+        }
+        return task_input
+    
+    def set_outputs(self, outputs:List):
+        while True:
+            self.rep_socket.recv(outputs, self.keep_alive)
+
+
+class Task(CoreIO):
+    def run(self):
+        try:
+            # get task inputs
+            inputs:List = self.get_inputs()
+
+            # Get the ABNF definition for the EnergyWeb DID
+            DID:str = "did:{}:{}".format(inputs["method"], inputs["address"])
+
+            # set task outputs
+            self.set_outputs([DID.encode("utf-8")])
+        except Exception as error:
+            logging.error(error)
+
+
+async def main():   
+    print("Running task")
+    threading.Thread(target=Task(name="get_device_DID", keep_alive=False).run).start()
+
+
+asyncio.run(main())
